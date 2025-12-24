1ee1177051526bbe689ccf010d9c71f3a63fcefa
maki-ni <nehemiahelias14@gmail.com>
2025-12-24 16:57:25 +0300

    edited the main function to export the files as csv files

--- main.cpp
+++ main.cpp
@@ -7,7 +7,17 @@ int main()
 {
     DrugBST dr;
     PatientQueue pq;
+    // Added drugs
     dr.addDrug("iboprofien");
     dr.addDrug("paracetamol");
     dr.displayDrugs();
+
+    // patients added
+    pq.enqueue(1, "Alice");
+    pq.enqueue(2, "Bob");
+    pq.display();
+
+    // Export current data to files
+    dr.exportToFile("drugs.csv");
+    pq.exportToFile("patients.csv");
 }
